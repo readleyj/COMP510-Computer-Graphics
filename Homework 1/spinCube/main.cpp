8b4b5b7789ed1d09d698898cc60edc82306998b0
Ismayil Ismayilov <iismayilov4574@gmail.com>
2022-03-14 16:47:44 +0300

    Separate out cube data into a separate namespace

--- Homework 1/spinCube/main.cpp
+++ Homework 1/spinCube/main.cpp
@@ -3,10 +3,82 @@
 typedef vec4 color4;
 typedef vec4 point4;
 
-const int NumVertices = 36;
+namespace cubeContext
+{
+    const int NumVertices = 36;
 
-point4 points[NumVertices];
-color4 colors[NumVertices];
+    point4 points[NumVertices];
+    color4 colors[NumVertices];
+
+    point4 vertices[8] = {
+        point4(-0.5, -0.5, 0.5, 1.0),
+        point4(-0.5, 0.5, 0.5, 1.0),
+        point4(0.5, 0.5, 0.5, 1.0),
+        point4(0.5, -0.5, 0.5, 1.0),
+        point4(-0.5, -0.5, -0.5, 1.0),
+        point4(-0.5, 0.5, -0.5, 1.0),
+        point4(0.5, 0.5, -0.5, 1.0),
+        point4(0.5, -0.5, -0.5, 1.0),
+    };
+
+    color4 vertex_colors[8] = {
+        color4(0.0, 0.0, 0.0, 1.0), // black
+        color4(1.0, 0.0, 0.0, 1.0), // red
+        color4(1.0, 1.0, 0.0, 1.0), // yellow
+        color4(0.0, 1.0, 0.0, 1.0), // green
+        color4(0.0, 0.0, 1.0, 1.0), // blue
+        color4(1.0, 0.0, 1.0, 1.0), // magenta
+        color4(1.0, 1.0, 1.0, 1.0), // white
+        color4(0.0, 1.0, 1.0, 1.0)  // cyan
+    };
+
+    // quad generates two triangles for each face and assigns colors
+    //    to the vertices
+
+    int Index = 0;
+
+    void quad(int a, int b, int c, int d)
+    {
+        colors[Index] = vertex_colors[a];
+        points[Index] = vertices[a];
+        Index++;
+
+        colors[Index] = vertex_colors[b];
+        points[Index] = vertices[b];
+        Index++;
+
+        colors[Index] = vertex_colors[c];
+        points[Index] = vertices[c];
+        Index++;
+
+        colors[Index] = vertex_colors[a];
+        points[Index] = vertices[a];
+        Index++;
+
+        colors[Index] = vertex_colors[c];
+        points[Index] = vertices[c];
+        Index++;
+
+        colors[Index] = vertex_colors[d];
+        points[Index] = vertices[d];
+        Index++;
+    }
+
+    // generate 12 triangles: 36 vertices and 36 colors
+    void colorcube()
+    {
+        quad(1, 0, 3, 2);
+        quad(2, 3, 7, 6);
+        quad(3, 0, 4, 7);
+        quad(6, 5, 1, 2);
+        quad(4, 5, 6, 7);
+        quad(5, 4, 0, 1);
+    }
+}
+
+namespace sphereContext
+{
+}
 
 enum BALL_SHAPE
 {
@@ -22,83 +94,15 @@ enum DRAWING_MODE
     WIREFRAME
 };
 
-point4 vertices[8] = {
-    point4(-0.5, -0.5, 0.5, 1.0),
-    point4(-0.5, 0.5, 0.5, 1.0),
-    point4(0.5, 0.5, 0.5, 1.0),
-    point4(0.5, -0.5, 0.5, 1.0),
-    point4(-0.5, -0.5, -0.5, 1.0),
-    point4(-0.5, 0.5, -0.5, 1.0),
-    point4(0.5, 0.5, -0.5, 1.0),
-    point4(0.5, -0.5, -0.5, 1.0),
-};
-
-color4 vertex_colors[8] = {
-    color4(0.0, 0.0, 0.0, 1.0), // black
-    color4(1.0, 0.0, 0.0, 1.0), // red
-    color4(1.0, 1.0, 0.0, 1.0), // yellow
-    color4(0.0, 1.0, 0.0, 1.0), // green
-    color4(0.0, 0.0, 1.0, 1.0), // blue
-    color4(1.0, 0.0, 1.0, 1.0), // magenta
-    color4(1.0, 1.0, 1.0, 1.0), // white
-    color4(0.0, 1.0, 1.0, 1.0)  // cyan
-};
-
 GLuint vao[NUM_SHAPES];
 
 // Model-view and projection matrices uniform location
 GLuint ModelView, Projection;
 
-//----------------------------------------------------------------------------
-
-// quad generates two triangles for each face and assigns colors
-//    to the vertices
-
-int Index = 0;
-
-void quad(int a, int b, int c, int d)
-{
-    // Initialize colors
-
-    colors[Index] = vertex_colors[a];
-    points[Index] = vertices[a];
-    Index++;
-    colors[Index] = vertex_colors[b];
-    points[Index] = vertices[b];
-    Index++;
-    colors[Index] = vertex_colors[c];
-    points[Index] = vertices[c];
-    Index++;
-    colors[Index] = vertex_colors[a];
-    points[Index] = vertices[a];
-    Index++;
-    colors[Index] = vertex_colors[c];
-    points[Index] = vertices[c];
-    Index++;
-    colors[Index] = vertex_colors[d];
-    points[Index] = vertices[d];
-    Index++;
-}
-
-//----------------------------------------------------------------------------
-
-// generate 12 triangles: 36 vertices and 36 colors
-void colorcube()
-{
-    quad(1, 0, 3, 2);
-    quad(2, 3, 7, 6);
-    quad(3, 0, 4, 7);
-    quad(6, 5, 1, 2);
-    quad(4, 5, 6, 7);
-    quad(5, 4, 0, 1);
-}
-
-//----------------------------------------------------------------------------
-
 // OpenGL initialization
 void init()
 {
-    colorcube();
+    cubeContext::colorcube();
 
     // Create a vertex array object
     glGenVertexArrays(2, vao);
@@ -108,9 +112,9 @@ void init()
     GLuint buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(points) + sizeof(colors), NULL, GL_STATIC_DRAW);
-    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(points), points);
-    glBufferSubData(GL_ARRAY_BUFFER, sizeof(points), sizeof(colors), colors);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeContext::points) + sizeof(cubeContext::colors), NULL, GL_STATIC_DRAW);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(cubeContext::points), cubeContext::points);
+    glBufferSubData(GL_ARRAY_BUFFER, sizeof(cubeContext::points), sizeof(cubeContext::colors), cubeContext::colors);
 
     // Load shaders and use the resulting shader program
     GLuint program = InitShader("vshader.glsl", "fshader.glsl");
@@ -122,7 +126,7 @@ void init()
 
     GLuint vColor = glGetAttribLocation(program, "vColor");
     glEnableVertexAttribArray(vColor);
-    glVertexAttribPointer(vColor, 4, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(sizeof(points)));
+    glVertexAttribPointer(vColor, 4, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(sizeof(cubeContext::points)));
 
     // Retrieve transformation uniform variable locations
     ModelView = glGetUniformLocation(program, "ModelView");
@@ -155,7 +159,7 @@ void display(void)
 
     glUniformMatrix4fv(ModelView, 1, GL_TRUE, model_view);
 
-    glDrawArrays(GL_TRIANGLES, 0, NumVertices);
+    glDrawArrays(GL_TRIANGLES, 0, cubeContext::NumVertices);
 
     glutSwapBuffers();
 }
